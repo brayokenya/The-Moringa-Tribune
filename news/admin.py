270714ae545800eef33e45accb02e7f9286c824f
brayokenya <kiirubrian21@gmail.com>
2020-06-24 14:46:20 +0300

    final adjustments

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Editor,Article,tag
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal =('tag',)
+
+admin.site.register(Editor)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(tag)
